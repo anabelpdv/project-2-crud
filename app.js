1449cbdb7c0314d15829f5f109943440c51cf346
Anabel Perez <anabelpdv@gmail.com>
2019-09-09 19:11:20 -0400

    Deployment

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const flash        = require("connect-flash");
 
 mongoose
   //.connect(`mongodb://localhost/${process.env.mongoDb}`, {useNewUrlParser: true})
-  .connect(process.env.MONGO_URI, {useNewUrlParser: true})
+  .connect(process.env.MONGODB_URI, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
